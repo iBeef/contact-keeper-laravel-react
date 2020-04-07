aa03c71153dc7380a1b7412689b12a05fcb31ad3
Lewis Holmes <ibeef1990@gmail.com>
2020-04-07 08:46:19 +0100

    Added ContactResource and condensed all contact routes into a Route::resource

--- routes/api/v1/contacts.php
+++ routes/api/v1/contacts.php
@@ -7,41 +7,14 @@
 */
 
 /**
- * Get all users contacts
+ * Contact routes
  * 
  * @route GET api/v1/contacts
- * @access private
- */
-Route::get('/', function() {
-    return "Get all contacts.";
-});
-
-/**
- * Add new contact
- * 
  * @route POST api/v1/contacts
+ * @route PUT api/v1/contacts/{$id}
+ * @route DELETE api/v1/contacts/{$id}
  * @access private
  */
-Route::post('/', function() {
-    return "Add a contact";
-});
-
-/**
- * Update a contact
- * 
- * @route PUT api/v1/contacts/{id}
- * @access private
- */
-Route::put('/{id}', function($id) {
-    return "Update contact";
-});
-
-/**
- * Delete a contact
- * 
- * @route DELETE api/v1/contacts/{id}
- * @access private
- */
-Route::delete('/{id}', function($id) {
-    return "Delete contact";
-});
+Route::resource('/', 'ContactController', [
+    'except' => ['edit', 'create']
+]);
